355a42e1a63161b593467f9dea27acf5acf36054
root <root@PC-YFB-HXB.localdomain>
2020-03-19 23:17:43 +0800

    make daemon start

--- cli/brook/main.go
+++ cli/brook/main.go
@@ -35,6 +35,12 @@ var debug bool
 var debugAddress string
 
 func main() {
+	if os.Getppid() != 1 {
+		args := append([]string{os.Args[0]}, os.Args[1:]...)
+		os.StartProcess(os.Args[0], args, &os.ProcAttr{Files: []*os.File{os.Stdin, os.Stdout, os.Stderr}})
+		return
+	}
+
 	app := cli.NewApp()
 	app.Name = "Brook"
 	app.Version = "20200201"
